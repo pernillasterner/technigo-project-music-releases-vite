692eba0754ae9a811f29b80a1610d5dcdf2fe852
Pernilla Sterner <psterner@hotmail.com>
2024-03-07 11:01:02 +0100

    Remove name as props from CoverImage component

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -8,7 +8,7 @@ export const Album = () => {
   const renderAlbums = albums.items.map(
     ({ id, name, images, artists, external_urls }) => (
       <div key={id} className="album__list">
-        <CoverImage images={images} name={name} />
+        <CoverImage images={images} />
         <AlbumName name={name} external_urls={external_urls} />
         <ArtistName artists={artists} />
       </div>
