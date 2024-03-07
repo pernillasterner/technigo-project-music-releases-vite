a27a2afd6506a44cb7ad6a7d3b2ec2b21ecb8454
Pernilla Sterner <psterner@hotmail.com>
2024-03-07 10:36:18 +0100

    Import only the album object part. Add box shadow to albums

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -2,13 +2,17 @@ import "./Album.css";
 import { AlbumName } from "./AlbumName";
 import { ArtistName } from "./ArtistName";
 import { CoverImage } from "./CoverImage";
+import { albums } from "../../data/albums.json";
 
-export const Album = ({ id, name, artists, external_urls, images }) => {
-  return (
-    <div className="album__list" key={id}>
-      <CoverImage images={images} name={name} />
-      <AlbumName name={name} external_urls={external_urls} />
-      <ArtistName artists={artists} />
-    </div>
+export const Album = () => {
+  const renderAlbums = albums.items.map(
+    ({ id, name, images, artists, external_urls }) => (
+      <div key={id} className="album__list">
+        <CoverImage images={images} name={name} />
+        <AlbumName name={name} external_urls={external_urls} />
+        <ArtistName artists={artists} />
+      </div>
+    )
   );
+  return <>{renderAlbums}</>;
 };
