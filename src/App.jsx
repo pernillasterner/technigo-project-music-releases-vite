4b14b1fad5814cc6f0038e8058cad55bc2d9b3e2
Pernilla Sterner <psterner@hotmail.com>
2024-03-07 21:48:54 +0100

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import { Album } from "./components/Album";
-import { Hero } from "./components/Header/Hero";
+import { Hero } from "./components/Hero/Hero";
 import { Playlist } from "./components/Playlist";
-import { albums } from "./data/albums.json";
 
 export const App = () => {
-  const renderHero = albums.items
-    .slice(10, 11)
-    .map(({ id, images, artists }) => (
-      <Hero key={id} images={images[0]} artists={artists} />
-    ));
-
   return (
     <main>
-      {renderHero}
-      <section className="secion__playlist">
-        <Playlist />
-      </section>
-      <section className="section__album">
-        <Album />
-      </section>
+      <Hero />
+      <Playlist />
+      <Album />
     </main>
   );
 };
