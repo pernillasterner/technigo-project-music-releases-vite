04322ca6a4229a528291401c3c7e67906fd809b8
Pernilla Sterner <psterner@hotmail.com>
2024-03-07 11:07:57 +0100

    Add propTypes name and url

--- src/components/Album/AlbumName.jsx
+++ src/components/Album/AlbumName.jsx
@@ -1,9 +1,21 @@
+import PropTypes from "prop-types";
+
 export const AlbumName = ({ name, external_urls }) => {
+  const albumInfo = {
+    url: external_urls.spotify,
+    name: name,
+  };
+
   return (
     <>
-      <a href={external_urls.spotify} className="album-title" target="_blank">
-        {name}
+      <a href={albumInfo.url} className="album-title" target="_blank">
+        {albumInfo.name}
       </a>
     </>
   );
+};
+
+AlbumName.propTypes = {
+  name: PropTypes.string.isRequired,
+  external_urls: PropTypes.object.isRequired,
 };
