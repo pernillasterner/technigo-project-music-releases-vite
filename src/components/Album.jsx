39f953653d2365418dc409156eaba9b39b0273e8
Pernilla Sterner <psterner@hotmail.com>
2024-03-07 21:44:43 +0100

    Clean up

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -8,11 +8,11 @@ export const Album = () => {
   const renderAlbums = albums.items.map(
     ({ id, name, images, artists, external_urls }) => (
       <div key={id} className="album__list">
-        <CoverImage images={images} />
+        <CoverImage images={images[1].url} />
         <AlbumName name={name} external_urls={external_urls} />
         <ArtistName artists={artists} />
       </div>
     )
   );
-  return <>{renderAlbums}</>;
+  return <section className="section__album">{renderAlbums}</section>;
 };
