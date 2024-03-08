bd48d5331edc0154f86472c937ac8502f77b6735
Pernilla Sterner <psterner@hotmail.com>
2024-03-08 11:38:28 +0100

    Seperate singles and albums. Adjust styling

--- src/components/AlbumList/AlbumList.jsx
+++ src/components/AlbumList/AlbumList.jsx
@@ -5,16 +5,37 @@ import { CoverImage } from "../AlbumContent/CoverImage";
 import { albums } from "../../data/albums.json";
 
 export const Album = () => {
-  console.log(albums);
-  const renderAlbums = albums.items.map(
-    ({ id, name, images, artists, external_urls }) => (
+  // Seperate singles and albums in two seperate arrays
+  const allSingles = albums.items.filter(
+    (album) => album.album_type === "single"
+  );
+
+  const allAlbums = albums.items.filter(
+    (album) => album.album_type !== "single"
+  );
+
+  const renderAlbums = (allMusic) =>
+    allMusic.map(({ id, name, images, artists, external_urls }) => (
       <div key={id} className="album__list">
         <CoverImage images={images[1].url} />
         <AlbumName name={name} external_urls={external_urls} />
         <ArtistName artists={artists} />
       </div>
-    )
-  );
+    ));
 
-  return <section className="section__album">{renderAlbums}</section>;
+  return (
+    <section className="section__album">
+      <div className="single-wrapper">
+        <h2>New singles</h2>
+        {/* Passing single array to renderAlbums */}
+        <div className="single-container">{renderAlbums(allSingles)}</div>
+      </div>
+
+      <div className="album-wrapper">
+        <h2>New albums</h2>
+        {/* Passing album array to renderAlbums */}
+        <div className="album-container">{renderAlbums(allAlbums)}</div>
+      </div>
+    </section>
+  );
 };
